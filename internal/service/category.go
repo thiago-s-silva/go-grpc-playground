13ee2191ce50ac3e167e4ab12534003129d469bf
thiago-s-silva <thiago.pfe@gmail.com>
2024-07-22 12:46:26 -0300

    Add the GetAll categories service

--- internal/service/category.go
+++ internal/service/category.go
@@ -37,3 +37,34 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 
 	return &res, nil
 }
+
+func (c *CategoryService) ListCategories(context.Context, *pb.Blank) (*pb.CategoryList, error) {
+	// Get all categories from DB
+	dbCategories, err := c.categoryRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Define a new variable to store the pb categories (gRPC format)
+	var categories []*pb.Category
+
+	// For each db category found
+	for _, category := range dbCategories {
+		// Create a new variable to parse the db category to a pb category
+		categoryRes := pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		}
+
+		// Append to the pb categories list
+		categories = append(categories, &categoryRes)
+	}
+
+	// Define a res variable following the pb service DTO response
+	res := &pb.CategoryList{
+		Categories: categories,
+	}
+
+	return res, nil
+}
